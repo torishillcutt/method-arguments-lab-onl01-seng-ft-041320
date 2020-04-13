521c9f6501b47881907f36bc40d520e4157833ae
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-13 22:51:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,3 +4,8 @@ end
 
 introduction("Josh")
   
+def introduction_with_language(name, language)
+  puts ("Hi, my name is #{name} and I am learning to program in #{language}.")
+end
+
+introduction_with_language("Dan", "Ember.js")
